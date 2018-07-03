829572cdc7ed0cf515feb17cee92a72946d3c6f2
Thomas H Jones II <github@xanthia.com>
2018-07-03 12:54:33 -0400

    Add basic project scaffolding

---
+++ Misc/NOOP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# PLACEHOLDER - this file exists so Travis doesn't faile the 
+# early commits due to lack of .sh files
+#
+#################################################################
