96c8b814d69aeee31c0d8bc03d2eb3a54f4e0526
Thomas H Jones II <github@xanthia.com>
2018-08-07 13:51:14 -0400

    Ensure access to CSP resources

--- TestStuff/Jenkins/standalone-CFn-IAM.groovy
+++ TestStuff/Jenkins/standalone-CFn-IAM.groovy
@@ -24,6 +24,7 @@ pipeline {
          string(name: 'CfnStackRoot', description: 'Unique token to prepend to all stack-element names')
          string(name: 'BackupBucketArn', description: 'ARN of S3 Bucket to host Collibra backups')
          string(name: 'RolePrefix', description: 'Prefix to apply to IAM role to make things a bit prettier (optional)')
+         string(name: 'CloudwatchBucketName', description: 'Name of the S3 Bucket hosting the CloudWatch agent archive files')
     }
 
     stages {
@@ -43,6 +44,10 @@ pipeline {
                              {
                                  "ParameterKey": "RolePrefix",
                                  "ParameterValue": "${env.RolePrefix}"
+                             },
+                             {
+                                 "ParameterKey": "CloudwatchBucketName",
+                                 "ParameterValue": "${env.CloudwatchBucketName}"
                              }
                          ]
                    /
